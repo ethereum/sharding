40271009b21d21a63dafb678d6e4ada1cebe3f74
Mai-Hsuan Chia <kevin.mh.chia@gmail.com>
2017-07-13 12:21:03 +0000

    Added limit to the deposit size for the `deposit`
    Additionally,
    1. Modified tester in order to compatible with the deposit size limit
    2. Fixed a typo from return_addr to validation_code_addr in `withdraw`
    3. Removed some unneeded comments

--- sharding/validation_manager_contract.v.py
+++ sharding/validation_manager_contract.v.py
@@ -17,8 +17,18 @@ num_validators: public(num)
 # indexs of empty slots caused by the function `withdraw`
 empty_slots_stack: num[num]
 
-# the end index of the stack in empty_slots_stack
+# the top index of the stack in empty_slots_stack
 top: num
+
+# the exact deposit size which you have to deposit to become a validator
+deposit_size: wei_value
+
+
+def __init__():
+
+    self.num_validators = 0
+    self.top = 0
+    self.deposit_size = 100000000000000000000 # 10 ** 20 wei = 100 ETH
 
 def is_stack_empty() -> bool:
 
@@ -62,7 +72,7 @@ def release_validator_slot(index: num):
 
 def deposit(validation_code_addr: address, return_addr: address) -> num:
 
-    # TODO: check for deposit to be equaled to a certain amount of ETH
+    assert msg.value == self.deposit_size
     index = self.take_validators_empty_slot()
     self.validators[index] = {
         deposit: msg.value,
@@ -75,7 +85,7 @@ def deposit(validation_code_addr: address, return_addr: address) -> num:
 def withdraw(validator_index: num, sig: bytes <= 1000) -> bool:
 
     msg_hash = sha3("withdraw")
-    result = (extract32(raw_call(self.validators[validator_index].return_addr, concat(msg_hash, sig), gas=200000, outsize=32), 0) == as_bytes32(1))
+    result = (extract32(raw_call(self.validators[validator_index].validation_code_addr, concat(msg_hash, sig), gas=200000, outsize=32), 0) == as_bytes32(1))
     if result:
         send(self.validators[validator_index].return_addr, self.validators[validator_index].deposit)
         self.validators[validator_index] = {
@@ -91,7 +101,6 @@ def sample(block_number: num, shard_id: num, sig_index: num) -> address:
 
     zero_addr = 0x0000000000000000000000000000000000000000
 
-    # TODO: need to handle the situation when there are empty slots
     cycle = floor(decimal(block_number / 2500))
     cycle_seed = blockhash(cycle * 2500)
     seed = blockhash(block_number)
@@ -109,6 +118,10 @@ def sample(block_number: num, shard_id: num, sig_index: num) -> address:
 
     return zero_addr
 
+def get_deposit_size() -> wei_value:
+
+    return self.deposit_size
+
 """
 
 def sign(msg_hash, privkey):
@@ -125,6 +138,10 @@ return(~mload(0) == {})
     """.format(utils.checksum_encode(address))
     return validation_code
 
+# Must pay 100 ETH to become a validator
+deposit_size = 10 ** 20
+withdraw_msg_hash = utils.sha3("withdraw")
+
 c = t.Chain()
 
 k0_valcode_addr = c.tx(t.k0, '', 0, serpent.compile(mk_validation_code(t.a0)))
@@ -132,23 +149,30 @@ k1_valcode_addr = c.tx(t.k1, '', 0, serpent.compile(mk_validation_code(t.a1)))
 
 x = c.contract(validation_manager_code, language='viper')
 
-withdraw_msg_hash = utils.sha3("withdraw")
+c.mine(1, coinbase=t.a0)
+c.head_state.gas_limit = 10 ** 10
+c.head_state.set_balance(address=t.a0, value=deposit_size * 10)
+c.head_state.set_balance(address=t.a1, value=deposit_size * 10)
 
+# test deposit to fail when msg.value != deposit_size
+try:
+    x.deposit(k0_valcode_addr, k0_valcode_addr)
+    assert False
+except t.TransactionFailed:
+    pass
 # test withdraw to fail when no validator record
 assert not x.withdraw(0, sign(withdraw_msg_hash, t.k0))
 # test deposit working fine
-assert 0 == x.deposit(k0_valcode_addr, k0_valcode_addr)
-assert 1 == x.deposit(k1_valcode_addr, k1_valcode_addr)
+assert 0 == x.deposit(k0_valcode_addr, k0_valcode_addr, value=deposit_size, sender=t.k0)
+assert 1 == x.deposit(k1_valcode_addr, k1_valcode_addr, value=deposit_size, sender=t.k1)
 assert x.withdraw(0, sign(withdraw_msg_hash, t.k0))
 # test deposit using empty slots
-assert 0 == x.deposit(k0_valcode_addr, k0_valcode_addr)
+assert 0 == x.deposit(k0_valcode_addr, k0_valcode_addr, value=deposit_size, sender=t.k0)
 assert x.withdraw(1, sign(withdraw_msg_hash, t.k1))
 # test deposit working fine in the edge condition
-assert 1 == x.deposit(k1_valcode_addr, k1_valcode_addr)
+assert 1 == x.deposit(k1_valcode_addr, k1_valcode_addr, value=deposit_size, sender=t.k1)
 # test withdraw to fail when the signature is not corret
 assert not x.withdraw(1, sign(withdraw_msg_hash, t.k0))
-
-c.mine(1, coinbase=t.a0)
 
 # test sample
 assert x.withdraw(0, sign(withdraw_msg_hash, t.k0))
