150dc7141d04cf8306dfd74f2c0d82c01535fc96
mhchia <j84255801912@gmail.com>
2017-08-21 22:37:03 +0800

    Add a tester prototype for receipt-consuming-tx

---
+++ sharding/receipt_consuming_transaction_tester.py
@@ -0,0 +1,15 @@
+import pytest
+
+from ethereum.messages import apply_transaction
+
+from sharding.tools import tester as t
+
+
+@pytest.fixture
+def c():
+    for k, v in t.base_alloc.items():
+        t.base_alloc[k] = {'balance': 10 * 100 * utils.denoms.ether}
+    return t.Chain(alloc=t.base_alloc)
+
+
+
