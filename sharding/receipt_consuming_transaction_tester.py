cd9871cf1ccb3c8959f03a01624201911c50b534
mhchia <j84255801912@gmail.com>
2017-08-26 22:55:32 +0800

    Add a simplified `validate_transaction` and TODOs
    TODO
            - should we deduct the intrinsic_gas of the tx?
            - what should be checked further in
              `simplified_validate_transaction`?
            - test the situation when the to_addr is a contract_address

--- sharding/receipt_consuming_transaction_tester.py
+++ sharding/receipt_consuming_transaction_tester.py
@@ -7,7 +7,7 @@ from ethereum.transactions import Transaction
 
 from sharding.tools import tester as t
 from sharding.used_receipt_store_utils import call_get_used_receipts, get_urs_ct, get_urs_contract
-from sharding.validator_manager_utils import MessageFailed, call_msg, call_tx, get_valmgr_addr, get_valmgr_ct, mk_initiating_contracts
+from sharding.validator_manager_utils import MessageFailed, call_msg, call_tx, get_valmgr_addr, get_valmgr_ct, mk_initiating_contracts, mk_validation_code
 
 # config_string = 'eth.chain:info,eth.pb.msg:debug'
 # configure_logging(config_string=config_string)
@@ -41,7 +41,18 @@ def call_valmgr(state, func, args):
     return o[0] if len(o) == 1 else o
 
 
+def simplified_validate_transaction(state, tx):
+    '''A simplified and modified one from
+       `ethereum.messages.validate_transction`
+       Every check involved in tx.sender is removed.
+    '''
+    if state.gas_used + tx.startgas > state.gas_limit:
+        return False
+    return True
+
+
 def is_receipt_consuming_tx_valid(state, shard_id, tx):
+    assert simplified_validate_transaction(state, tx)
     if (tx.v != 1) or (tx.s != 0) or not isinstance(tx.r, int):
         return False
     receipt_id = tx.r
@@ -85,13 +96,15 @@ def send_msg_transfer_value(state, shard_id, tx):
     # XXX: we should deduct the startgas of this message in advance, because
     #      the message may be possibly a contract, not only a normal value
     #      transfer.
+    # TODO: should we deduct the intrinsic_gas of the tx, which is calculated
+    #       in `apply_transaction` here?
     value = tx.value - tx.gasprice * tx.startgas
+    log_rctx.debug("value={}, tx.value={}, tx.gasprice={}, tx.startgas={}".format(value, tx.value, tx.gasprice, tx.startgas))
     if value <= 0:
         return False
+
     # start transactioning
     send_msg_add_receipt(state, shard_id, receipt_id)
-
-    log_rctx.debug("value={}, tx.value={}, tx.gasprice={}, tx.startgas={}".format(value, tx.value, tx.gasprice, tx.startgas))
 
     receipt_sender_hex = call_valmgr(state, 'get_receipts__sender', [receipt_id])
     receipt_data = call_valmgr(state, 'get_receipts__data', [receipt_id])
@@ -108,11 +121,13 @@ def send_msg_transfer_value(state, shard_id, tx):
     log_rctx.debug("after apply_msg:  urs_addr.balance={}, tx.to.balance={}".format(state.get_balance(urs_addr), state.get_balance(tx.to)))
     # result_data = bytearray_to_bytestr(data) if result else None
     if not result:
+        # TODO: is it correct to revert the balance here?
         state.delta_balance(urs_addr, -value)
         raise MessageFailed("send_msg_transfer_value: failed")
 
     # gas refunds goes to the `to` address
     refunds = gas_remained * tx.gasprice
+    log_rctx.debug("gas_remained={}, gasprice={}".format(gas_remained, tx.gasprice))
     state.delta_balance(to, refunds)
     log_rctx.debug("End: urs.balance={}, tx.to.balance={}\n\n".format(state.get_balance(urs_addr), state.get_balance(tx.to)))
 
@@ -120,7 +135,7 @@ def send_msg_transfer_value(state, shard_id, tx):
 
 
 def mk_testing_receipt_consuming_tx(receipt_id, to_addr, value, data=b''):
-    startgas = 21000
+    startgas = 210000
     tx = Transaction(0, t.GASPRICE, startgas, to_addr, value, data)
     tx.v, tx.r, tx.s = 1, receipt_id, 0
     return tx
@@ -134,6 +149,11 @@ def test_receipt_consuming_transaction(c):
     c.mine(1)
     valmgr = t.ABIContract(c, get_valmgr_ct(), get_valmgr_addr())
     to_addr = utils.privtoaddr(utils.sha3("test_to_addr"))
+    '''
+    # test the case when the to_addr is a contract_address
+    valcode_addr = c.tx(to=b'', data=mk_validation_code(t.a9))
+    to_addr = valcode_addr
+    '''
     # registre receipts
     valmgr.tx_to_shard(to_addr, shard_id, b'', sender=t.k0, value=1)
     value = 500000
@@ -145,13 +165,7 @@ def test_receipt_consuming_transaction(c):
     c.mine(1)
     assert not urs0.get_used_receipts(receipt_id)
     c.mine(1)
-    '''
-    send_msg_add_receipt(c.head_state, shard_id, receipt_id)
-    send_msg_add_receipt(c.chain.state, shard_id, receipt_id)
-    # urs0.add_used_receipt(receipt_id)
-    assert urs0.get_used_receipts(receipt_id)
-    c.mine(1)
-    '''
+
     tx = mk_testing_receipt_consuming_tx(receipt_id, to_addr, value)
     if is_receipt_consuming_tx_valid(c.head_state, shard_id, tx):
         send_msg_transfer_value(c.head_state, shard_id, tx)
