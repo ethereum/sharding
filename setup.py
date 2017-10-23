394521d87b82972330e77cf085d18be256cbc824
Hsiao-Wei Wang <hwwang156@gmail.com>
2017-10-23 16:10:52 +0800

    Bump dependency packages version

--- setup.py
+++ setup.py
@@ -23,13 +23,13 @@ INSTALL_REQUIRES = list(set(INSTALL_REQUIRES))
 DEPENDENCY_LINKS = []
 if os.environ.get("USE_PYETHEREUM_DEVELOP"):
     # Force installation of specific commits of pyethereum.
-    pyethereum_ref = '4e945e2a24554ec04eccb160cff689a82eed7e0d'
+    pyethereum_ref = 'ed66e318b8dc34af925096846a77133b201d64b4'   # Oct 22, 2017
     DEPENDENCY_LINKS = [
         'http://github.com/ethereum/pyethereum/tarball/%s#egg=ethereum-9.99.9' % pyethereum_ref
     ]
 
 # Force installation of specific commits of viper.
-viper_ref = '12847483f571505cef72fcb725e6557db15bce29'  # Sep 12, 2017
+viper_ref = '9a6f972ba459f66e63adcfe9a4ad1c7d2f6ec47a'  # Oct 23, 2017
 DEPENDENCY_LINKS.append('http://github.com/ethereum/viper/tarball/%s#egg=viper-9.99.9' % viper_ref)
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
