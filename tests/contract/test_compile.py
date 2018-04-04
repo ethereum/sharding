8814badbbcbb9712ef62260fa7f1b94cf56bba9b
NIC619 <twedusuck@gmail.com>
2018-04-05 00:09:39 +0800

    Add smc compile test and handler tests

---
+++ tests/contract/test_compile.py
@@ -0,0 +1,18 @@
+from vyper import compiler
+
+from contracts.utils.smc_utils import (
+    get_smc_json,
+    get_smc_source_code,
+)
+
+
+def test_compile_smc():
+    compiled_smc_json = get_smc_json()
+
+    vmc_code = get_smc_source_code()
+    abi = compiler.mk_full_signature(vmc_code)
+    bytecode = compiler.compile(vmc_code)
+    bytecode_hex = '0x' + bytecode.hex()
+
+    assert abi == compiled_smc_json["abi"]
+    assert bytecode_hex == compiled_smc_json["bytecode"]
