3ea3c3c9737cdfc4c1c2af8aaf58aeb84c731392
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-04-28 13:22:39 -0400

    Added sharding/__init__.py

---
+++ sharding/__init__.py
@@ -0,0 +1,19 @@
+import pkg_resources
+
+from sharding.contracts.utils.smc_utils import (  # noqa: F401
+    get_smc_source_code,
+    get_smc_json,
+)
+
+from sharding.handler.log_handler import (  # noqa: F401
+    LogHandler,
+)
+from sharding.handler.shard_tracker import (  # noqa: F401
+    ShardTracker,
+)
+from sharding.handler.smc_handler import (  # noqa: F401
+    SMCHandler,
+)
+
+
+__version__ = pkg_resources.get_distribution("sharding").version
