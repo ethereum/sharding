325ba9a9e7f4878a6f89595858975762b47ef5f7
NIC619 <twedusuck@gmail.com>
2018-05-08 09:36:13 +0800

    Adjust PERIOD_LENGTH and NOTARY_LOCKUP_LENGTH in testing config

--- tests/handler/utils/config.py
+++ tests/handler/utils/config.py
@@ -10,10 +10,10 @@ from sharding.contracts.utils.config import (
 def get_sharding_testing_config():
     REPLACED_PARAMETERS = {
         'SHARD_COUNT': 10,
-        'PERIOD_LENGTH': 5,
+        'PERIOD_LENGTH': 10,
         'COMMITTEE_SIZE': 6,
         'QUORUM_SIZE': 4,
-        'NOTARY_LOCKUP_LENGTH': 120,
+        'NOTARY_LOCKUP_LENGTH': 60,
     }
     return merge(
         get_sharding_config(),
