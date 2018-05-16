64911af0de1cdcf8aeb96216157eb84be6a0b4f6
NIC619 <twedusuck@gmail.com>
2018-05-16 18:49:17 +0900

    Replace log fetching in log emission test with ShardTracker functions

--- tests/contract/test_log_emission.py
+++ tests/contract/test_log_emission.py
@@ -1,3 +1,9 @@
+from sharding.handler.log_handler import (  # noqa: F401
+    LogHandler,
+)
+from sharding.handler.shard_tracker import (  # noqa: F401
+    ShardTracker,
+)
 from sharding.handler.utils.web3_utils import (
     mine,
 )
@@ -17,20 +23,25 @@ from tests.contract.utils.sample_helper import (
 
 def test_log_emission(smc_handler):  # noqa: F811
     web3 = smc_handler.web3
+    log_handler = LogHandler(w3=web3, history_size=1000)
+    shard_tracker = ShardTracker(
+        shard_id=0,
+        log_handler=log_handler,
+        smc_handler_address=smc_handler.address,
+    )
     notary = NotaryAccount(0)
 
     # Register
-    tx_hash = smc_handler.register_notary(private_key=notary.private_key)
+    smc_handler.register_notary(private_key=notary.private_key)
     mine(web3, 1)
     # Check that log was successfully emitted
-    tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-    log = smc_handler.events.RegisterNotary().processReceipt(tx_receipt)[0]['args']
+    log = shard_tracker.get_register_notary_logs()[0]
     assert log['index_in_notary_pool'] == 0 and log['notary'] == notary.checksum_address
     fast_forward(smc_handler, 1)
 
     # Add header
     CHUNK_ROOT_1_0 = b'\x10' * 32
-    tx_hash = smc_handler.add_header(
+    smc_handler.add_header(
         period=1,
         shard_id=0,
         chunk_root=CHUNK_ROOT_1_0,
@@ -38,14 +49,13 @@ def test_log_emission(smc_handler):  # noqa: F811
     )
     mine(web3, 1)
     # Check that log was successfully emitted
-    tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-    log = smc_handler.events.AddHeader().processReceipt(tx_receipt)[0]['args']
+    log = shard_tracker.get_add_header_logs()[0]
     assert log['period'] == 1 and log['shard_id'] == 0 and log['chunk_root'] == CHUNK_ROOT_1_0
 
     # Submit vote
     sample_index = 0
     pool_index = sampling(smc_handler, 0)[sample_index]
-    tx_hash = smc_handler.submit_vote(
+    smc_handler.submit_vote(
         period=1,
         shard_id=0,
         chunk_root=CHUNK_ROOT_1_0,
@@ -54,27 +64,24 @@ def test_log_emission(smc_handler):  # noqa: F811
     )
     mine(web3, 1)
     # Check that log was successfully emitted
-    tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-    log = smc_handler.events.SubmitVote().processReceipt(tx_receipt)[0]['args']
+    log = shard_tracker.get_submit_vote_logs()[0]
     assert log['period'] == 1 and log['shard_id'] == 0 and log['chunk_root'] == CHUNK_ROOT_1_0 \
         and log['notary'] == NotaryAccount(pool_index).checksum_address
     fast_forward(smc_handler, 1)
 
     # Deregister
-    tx_hash = smc_handler.deregister_notary(private_key=notary.private_key)
+    smc_handler.deregister_notary(private_key=notary.private_key)
     mine(web3, 1)
     # Check that log was successfully emitted
-    tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-    log = smc_handler.events.DeregisterNotary().processReceipt(tx_receipt)[0]['args']
+    log = shard_tracker.get_deregister_notary_logs()[0]
     assert log['index_in_notary_pool'] == 0 and log['notary'] == notary.checksum_address \
         and log['deregistered_period'] == 2
     # Fast foward to end of lock up
     fast_forward(smc_handler, smc_handler.config['NOTARY_LOCKUP_LENGTH'] + 1)
 
     # Release
-    tx_hash = smc_handler.release_notary(private_key=notary.private_key)
+    smc_handler.release_notary(private_key=notary.private_key)
     mine(web3, 1)
     # Check that log was successfully emitted
-    tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-    log = smc_handler.events.ReleaseNotary().processReceipt(tx_receipt)[0]['args']
+    log = shard_tracker.get_release_notary_logs()[0]
     assert log['index_in_notary_pool'] == 0 and log['notary'] == notary.checksum_address
