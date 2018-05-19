d1fb61bddd0fb2ed69ec605c6a9fcb25184e6a82
NIC619 <twedusuck@gmail.com>
2018-05-20 00:57:12 +0800

    Add functions for user to check the status of registration or votes

--- sharding/handler/shard_tracker.py
+++ sharding/handler/shard_tracker.py
@@ -1,8 +1,13 @@
 from eth_utils import (
     encode_hex,
     to_list,
+    is_address,
+    to_checksum_address,
 )
 
+from sharding.contracts.utils.config import (
+    get_sharding_config,
+)
 from sharding.handler.utils.shard_tracker_utils import (
     parse_register_notary_log,
     parse_deregister_notary_log,
@@ -17,7 +22,11 @@ class ShardTracker:
     """Track emitted logs of specific shard.
     """
 
-    def __init__(self, shard_id, log_handler, smc_handler_address):
+    def __init__(self, config, shard_id, log_handler, smc_handler_address):
+        if config is None:
+            self.config = get_sharding_config()
+        else:
+            self.config = config
         self.shard_id = shard_id
         self.log_handler = log_handler
         self.smc_handler_address = smc_handler_address
@@ -84,3 +93,45 @@ class ShardTracker:
         logs = self._get_logs_by_shard_id(event_name='SubmitVote')
         for log in logs:
             yield parse_submit_vote_log(log)
+
+    #
+    # Functions for user to check the status of registration or votes
+    #
+    def is_notary_registered(self, notary):
+        assert is_address(notary)
+        # Normalize the notary address and padded to 32 bytes
+        normalized_address = '0x' + to_checksum_address(notary)[2:].rjust(64, '0')
+        log = self._get_logs_by_notary(event_name='RegisterNotary', notary=normalized_address)
+        return False if not log else True
+
+    def is_notary_deregistered(self, notary):
+        assert is_address(notary)
+        # Normalize the notary address and padded to 32 bytes
+        normalized_address = '0x' + to_checksum_address(notary)[2:].rjust(64, '0')
+        log = self._get_logs_by_notary(event_name='DeregisterNotary', notary=normalized_address)
+        return False if not log else True
+
+    def is_notary_released(self, notary):
+        assert is_address(notary)
+        # Normalize the notary address and padded to 32 bytes
+        normalized_address = '0x' + to_checksum_address(notary)[2:].rjust(64, '0')
+        log = self._get_logs_by_notary(event_name='ReleaseNotary', notary=normalized_address)
+        return False if not log else True
+
+    def is_new_header_added(self, period):
+        # Get the header added in the specified period
+        log = self._get_logs_by_shard_id(
+            event_name='AddHeader',
+            from_block=period * self.config['PERIOD_LENGTH'],
+            to_block=(period + 1) * self.config['PERIOD_LENGTH'] - 1,
+        )
+        return False if not log else True
+
+    def has_enough_vote(self, period):
+        # Get the votes submitted in the specified period
+        logs = self._get_logs_by_shard_id(
+            event_name='SubmitVote',
+            from_block=period * self.config['PERIOD_LENGTH'],
+            to_block=(period + 1) * self.config['PERIOD_LENGTH'] - 1,
+        )
+        return False if not logs else len(logs) >= self.config['QUORUM_SIZE']
