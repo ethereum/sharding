7ba986d31d0a1bfe5214ebaeec5bc698bd9421ef
NIC619 <twedusuck@gmail.com>
2018-05-20 00:57:00 +0800

    Fix log filter block number not being integer and add check

--- sharding/handler/log_handler.py
+++ sharding/handler/log_handler.py
@@ -20,13 +20,23 @@ class LogHandler:
         if from_block is None:
             # Search from the start of current period
             fromBlock = current_block_number - current_block_number % self.period_length
+        else:
+            if from_block > current_block_number:
+                raise Exception(
+                    "Can not search logs starting with block number"
+                    "larger than current block number"
+                )
+            fromBlock = from_block
         if to_block is None:
             toBlock = current_block_number
+        else:
+            toBlock = min(current_block_number, to_block)
 
         return self.web3.eth.getLogs(
             {
-                'fromBlock': fromBlock,
-                'toBlock': toBlock,
+                # Block number must be integer
+                'fromBlock': int(fromBlock),
+                'toBlock': int(toBlock),
                 'address': address,
                 'topics': topics,
             }
