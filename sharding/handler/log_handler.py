639e9d03535946fd9722a208ee6e2a41a432cbed
NIC619 <twedusuck@gmail.com>
2018-05-21 18:11:24 +0800

    Adjust the way of passing log filter parameters

--- sharding/handler/log_handler.py
+++ sharding/handler/log_handler.py
@@ -16,28 +16,29 @@ class LogHandler:
             topics=None,
             from_block=None,
             to_block=None):
+        filter_params = {
+            'address': address,
+            'topics': topics,
+        }
+
         current_block_number = self.w3.eth.blockNumber
         if from_block is None:
-            # Search from the start of current period
-            fromBlock = current_block_number - current_block_number % self.period_length
+            # Search from the start of current period if from_block is not given
+            filter_params['fromBlock'] = current_block_number - \
+                current_block_number % self.period_length
         else:
             if from_block > current_block_number:
                 raise Exception(
                     "Can not search logs starting with block number"
                     "larger than current block number"
                 )
-            fromBlock = from_block
-        if to_block is None:
-            toBlock = current_block_number
-        else:
-            toBlock = min(current_block_number, to_block)
+            # Block number must be integer
+            filter_params['fromBlock'] = int(from_block)
 
-        return self.w3.eth.getLogs(
-            {
-                # Block number must be integer
-                'fromBlock': int(fromBlock),
-                'toBlock': int(toBlock),
-                'address': address,
-                'topics': topics,
-            }
-        )
+        if to_block is None:
+            filter_params['toBlock'] = current_block_number
+        else:
+            # Block number must be integer
+            filter_params['toBlock'] = min(current_block_number, int(to_block))
+
+        return self.w3.eth.getLogs(filter_params)
