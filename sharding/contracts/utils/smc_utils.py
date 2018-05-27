41abaf4a843ee6ccd72578934abedc70ff3ae4d7
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-05-27 11:44:47 +0800

    Renamed `validator_manager` to `sharding_manager`

--- sharding/contracts/utils/smc_utils.py
+++ sharding/contracts/utils/smc_utils.py
@@ -6,12 +6,12 @@ DIR = os.path.dirname(__file__)
 
 
 def get_smc_source_code():
-    file_path = os.path.join(DIR, '../validator_manager.v.py')
+    file_path = os.path.join(DIR, '../sharding_manager.v.py')
     smc_source_code = open(file_path).read()
     return smc_source_code
 
 
 def get_smc_json():
-    file_path = os.path.join(DIR, '../validator_manager.json')
+    file_path = os.path.join(DIR, '../sharding_manager.json')
     smc_json_str = open(file_path).read()
     return json.loads(smc_json_str)
