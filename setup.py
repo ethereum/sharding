f80c08780ac25acf58775d0aff5f449848de5ca8
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-06-07 23:48:48 +0800

    Bump version: 0.0.1-alpha.3 → 0.0.2-alpha.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 setup(
     name='sharding',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.0.1-alpha.3',
+    version='0.0.2-alpha.1',
     description='Ethereum Sharding Manager Contract',
     url='https://github.com/ethereum/sharding',
     packages=find_packages(
